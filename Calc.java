79ee6920adeff90350ded22db4ebc4057c470279
Benny Wah <benw60615@gmail.com>
2022-05-18 14:43:07 -0500

    added the rest

---
+++ Calc.java
@@ -0,0 +1,17 @@
+public class Calc{
+	public static void main(String[] args){
+		public int(int n1, int n2){
+//			int n1,n2;
+//			int n3; // this is the sum
+
+			return n1+n2;
+	}
+                public int(int n1, int n2){
+//                      int n1,n2;
+//                      int n3; // this is the sum
+
+                        return n1-n2;
+        }
+}
+
+}
